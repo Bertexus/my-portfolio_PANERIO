7acf22ea1e27f8415e2f33b5840ffd1665ae4dcc
Bertexus <eribertopanerio@gmail.com>
2024-08-28 15:38:01 +0800

    This is a new comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='card bg-white rounded-lg'>
-        <h1 className="font-bold text-red-500">HELLO WORLD</h1>
-      </div>
+      
+        <h1 className="text-5xl font-extrabold">HELLO WORLD</h1>
+      
     </>
   )
 }
